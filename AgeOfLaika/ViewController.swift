2501984cabd9cf9c0d8a7bae711afbbfe5a16e21
Chris Tan <iChris@Chriss-MacBook-Air.local>
2015-05-26 22:20:31 +0800

    first rev

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearTextField: UITextField!
+    @IBOutlet weak var dogConvertedYearLabel: UILabel!
+    @IBOutlet weak var conversionButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dogConvertedButtonPressed(sender: UIButton) {
+        
+        let dogYearConstant = 7
+        let dogFromHumanYear = humanYearTextField.text.toInt()!
+        
+        dogConvertedYearLabel.hidden = false
+        humanYearTextField.resignFirstResponder()
+        
+        dogConvertedYearLabel.text = "Your Dog is " + "\(dogFromHumanYear * dogYearConstant)" + " years old"
+        
+        conversionButton.setTitle("Conversion Success", forState: UIControlState.Normal)
+        conversionButton.setTitleColor(UIColor .redColor(), forState: UIControlState.Normal)
+    }
+    
 }
 
